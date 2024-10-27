bbdc962aa17e1beffb7b583356d5b7508cdbb591
DiegoVargas0813 <A01635782@tec.mx>
2024-10-27 17:52:54 -0600

    Comments

--- main.cpp
+++ main.cpp
@@ -5,15 +5,15 @@ using namespace std;
 
 int knapsack(int W, vector<int>& weights, vector<int>& values, int n){ // Computational Complexity: O(n*W), where n is the number of items and W is the maximum weight capacity of the knapsack. Two loops are used, this implies a multiplication of n and w. Also, the space complexity is O(n*W), where the 2D Vector requires storing (n + 1) × (W + 1).
 
-    // Create a 2D vector to store the maximum benefit at each step
+    // Create matrix B
     vector<vector<int>> B(n + 1, vector<int>(W + 1, 0));
 
-    // Build the table B[][] in a bottom-up manner
+    // Build the matrix
     for (int i = 1; i <= n; ++i) {
         for (int w = 0; w <= W; ++w) {
-            // Check if the current item can be included in the knapsack
+            // Check if the item can be included
             if (weights[i - 1] <= w) {
-                // Include the current item and check if it gives a better value than excluding it
+                // Include the current item 
                 B[i][w] = max(values[i - 1] + B[i - 1][w - weights[i - 1]], B[i - 1][w]);
             } else {
                 // Exclude the current item
@@ -22,30 +22,31 @@ int knapsack(int W, vector<int>& weights, vector<int>& values, int n){ // Comput
         }
     }
 
-    // The answer will be in B[n][W], i.e., the maximum value for n items and total weight W
+    // Return best  value ratio
     return B[n][W];
 }
 
 int main() {
     int n, W;
     
-    // Input number of items and maximum weight capacity
+    // Input number of items 
     cout << "Enter the number of items: ";
     cin >> n;
 
     vector<int> weights(n), values(n);
-
+    //Input the value
     cout << "Enter the values of the items: ";
     for (int i = 0; i < n; i++) {
         cin >> values[i];
     }
 
-    // Input the weights and values of each item
+    // Input the weights 
     cout << "Enter the weights of the items: ";
     for (int i = 0; i < n; i++) {
         cin >> weights[i];
     }
 
+    //Max capacity
     cout << "Enter the maximum weight capacity of the knapsack: ";
     cin >> W;
 
